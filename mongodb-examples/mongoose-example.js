ea43cf7c5ae38afdc9c98c41aed70f442250c192
Fares <fares@somewhere.com>
2021-10-14 18:04:03 +0200

    small changes to mongoose example

--- mongodb-examples/mongoose-example.js
+++ mongodb-examples/mongoose-example.js
@@ -1,52 +1,67 @@
-(async function(){
-const mongoose = require('mongoose');
+(async function () {
+    const mongoose = require('mongoose');
 
-main()
-    .then(() => console.log(`connected`))
-    .catch(err => console.log(err));
-
-async function main() {
-    await mongoose.connect('mongodb://localhost:27017/test');
-}
-
-const kittySchema = new mongoose.Schema({
-    name: String
-});
-
-//compiling our schema into a Model.
-//const Kitten = mongoose.model('Kitten', kittySchema);
+    /*If you want to access the database as a particular user, 
+    you can add the username and password in front of the hostname.
+    var dbURI = 'mongodb://username:password@localhost/mydatabase';
+    **/
 
 
+    mongoose.connection
+        .on('error', (err) => console.log('Mongoose connection error: ' + err))
+        .on('connected', () => console.log('Mongoose connected'));
 
-// add "speak" functionality to our documents:
-// NOTE: methods must be added to the schema before compiling it with mongoose.model()
-kittySchema.methods.speak = function speak() {
-    const greeting = this.name
-        ? "Meow name is " + this.name
-        : "I don't have a name";
-    console.log(greeting);
-};
+    const dbURI = 'mongodb://localhost:27017/test';
+    const dbOptions = { 'user': 'root', 'pass': 'root' };
+
+    mongoose
+        .connect(dbURI)
+        //.connect(dbURI, dbOptions)
+        .then(() => console.log(`connected`))
+        .catch(err => console.log(err));
+
+    const kittySchema = new mongoose.Schema({
+        name: String
+    });
+
+
+    // add "speak" functionality to our documents:
+    // NOTE: methods must be added to the schema before compiling it with mongoose.model()
+    kittySchema.methods.speak = function speak() {
+        const greeting = this.name
+            ? "Meow name is " + this.name
+            : "I don't have a name";
+        console.log(greeting);
+    };
 
 
 
-const Kitten = mongoose.model('Kitten', kittySchema);
+    const Kitten = mongoose.model('Kitten', kittySchema);
 
-// create an example kitten
-const silence = new Kitten({ name: 'Silence' });
-console.log(silence.name); // 'Silence'
-/*Functions added to the methods property of a schema get compiled 
-into the Model prototype and exposed on each document instance:
-*/
-const fluffy = new Kitten({ name: 'fluffy' });
-fluffy.speak(); // "Meow name is fluffy"
+    // create an example kitten
+    const silence = new Kitten({ name: 'Silence' });
+    console.log(silence.name); // 'Silence'
+    /*Functions added to the methods property of a schema get compiled 
+    into the Model prototype and exposed on each document instance:
+    */
+    const fluffy = new Kitten({ name: 'fluffy' });
+    fluffy.speak(); // "Meow name is fluffy"
 
-//Each document can be saved to the database by calling its save method
-await fluffy.save();
-fluffy.speak();
-//We can access all of the kitten documents through our Kitten model.
-const kittens = await Kitten.find();
-console.log(kittens);
-// If we want to filter our kittens by name, Mongoose supports MongoDBs rich querying syntax.
-await Kitten.find({ name: /^fluff/ });
+    //Each document can be saved to the database by calling its save method
+    await fluffy.save();
+    fluffy.speak();
+    //We can access all of the kitten documents through our Kitten model.
+    const kittens = await Kitten.find();
+    console.log(kittens);
+    // If we want to filter our kittens by name, Mongoose supports MongoDBs rich querying syntax.
+    await Kitten.find({ name: /^fluff/ });
+
+    process.on('SIGINT', function () {
+        mongoose.connection.close(function () {
+            console.log('Mongoose disconnected through app termination');
+            process.exit(0);
+        });
+    });
+
 
 })()
