17cddab2247c770c5c00f0c2d2ffada764fde0a0
Fares <fares@somewhere.com>
2021-10-14 13:58:08 +0200

    add simple example of mongodb

---
+++ mongodb-examples/queryCustomers.js
@@ -0,0 +1,18 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  //address starts with the letter "S":
+  var query = { address: /^S/ };
+  var projection = { projection: { _id: 0, name: 1, address: 1 } };
+  dbo
+  .collection("customers")
+  .find(query, projection)
+  .toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    db.close();
+  });
+});
--- promise-demo.js
+++ promise-demo.js
@@ -1,3 +1,21 @@
+// newbie, using a promis
+const axios = require('axios');
+
+const url = 'www.google.com';
+let res = axios.get(url); 
+  res
+  .then(data=> data.response) // implicit return (everytime we use arrow function) 
+  .then(v => DoSomething(v))
+  // will happen after 2 sec
+  .then(noValue => {throw new Error('a problem')});// will happen after)
+
+  function DoSomething(data){
+      return new Promise( (res, rej) => {
+        setTimeout((data )=> console.log(v), 2000)
+      }
+         
+      )
+  }
 console.log(`Start ....`)
 
 let p = new Promise( 
